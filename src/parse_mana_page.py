c318dbeb557bd83f0ec124db9466cdb459a8e1be
mvk-mozilla <mvankleeck@mozilla.com>
2018-11-09 15:55:38 +0100

    Initial parsing and word counting

--- src/parse_mana_page.py
+++ src/parse_mana_page.py
@@ -1,10 +1,24 @@
 import codecs
 import nltk
+nltk.download('punkt')
+
 from bs4 import BeautifulSoup
+from nltk import word_tokenize
+from nltk.probability import ConditionalFreqDist
+
+#from matplotlib import pylab
 
 f = codecs.open("../data/testpage01.html", 'r', 'utf-8')
 document = BeautifulSoup(f.read()).get_text()
-[print(i) for i in document]
+#[print(i) for i in document]
 
+tokens = word_tokenize(document)
+print(len(tokens))
+wordList=list(set(tokens))
+
+tokeFreqs=dict.fromkeys(wordList,0)
+for word in tokens:
+   tokeFreqs[word]+=1
+print (tokeFreqs)
 
 
