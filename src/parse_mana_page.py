dcd3464347b5615006bdd236a325fd936945bbd5
Martin Lopatka <mlopatka@mlopatka-fmjhd3.local>
2018-11-09 15:23:36 +0100

    Add a basic html parse example

--- src/parse_mana_page.py
+++ src/parse_mana_page.py
@@ -1,3 +1,10 @@
+import codecs
 import nltk
-import beautiful_soup
+from bs4 import BeautifulSoup
+
+f = codecs.open("../data/testpage01.html", 'r', 'utf-8')
+document = BeautifulSoup(f.read()).get_text()
+[print(i) for i in document]
+
+
 
